4a06bc6a52952b815d24a45b9d5ba2411cc3f76a
Mory Friedman <friedmanmory2@gmail.com>
2015-12-03 13:31:19 -0600

    Fixed Formatting

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,8 @@ button.on('click', function() {
 	 button = prompt('What is your age?');
 
 	 if(button <= '15') {
-	 	console.log('You are not old enough to legally drive');
+	 	console.log('You are not old enough to legally drive.');
 	 } else {
-	 	console.log('You are old enough to legally drive');
+	 	console.log('You are old enough to legally drive.');
 	 }
 });
