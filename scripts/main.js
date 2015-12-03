345d874e9b077b0bebc845cd1cc5fcb7da35a2d4
Mory Friedman <friedmanmory2@gmail.com>
2015-12-03 13:25:21 -0600

    Legal Driving Age Project

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var form = $('#form');
+var button = $('#button');
+
+button.on('click', function() {
+	 button = prompt('What is your age?');
+
+	 if(button <= '15') {
+	 	console.log('You are not old enough to legally drive');
+	 } else {
+	 	console.log('You are old enough to legally drive')
+	 }
+});
