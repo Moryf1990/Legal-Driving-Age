cf3e34746d87e2a24f64f082f1bf04b8a2eefe4f
Mory Friedman <friedmanmory2@gmail.com>
2015-12-03 13:36:28 -0600

    Converting Age to a Number

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ var button = $('#button');
 button.on('click', function() {
 	 button = prompt('What is your age?');
 
-	 if(button <= '15') {
+	 if(button <= parseFloat(15)) {
 	 	console.log('You are not old enough to legally drive.');
 	 } else {
 	 	console.log('You are old enough to legally drive.');
