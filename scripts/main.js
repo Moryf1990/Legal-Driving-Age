64bfd4133caeb20abc4191e0e9f3602c1ccb7405
Mory Friedman <friedmanmory2@gmail.com>
2015-12-05 11:33:51 -0600

    Adding Styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var form = $('#form');
 var button = $('#button');
 
 button.on('click', function() {
-	 button = prompt('What is your age?');
+	 button = prompt('What is your age?', 'Enter your age here.');
 
 	 if(button <= parseFloat(15)) {
 	 	console.log('You are not old enough to legally drive.');
